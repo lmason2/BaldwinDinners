cca9f75cd353d373bd77fc780efb11c4732f1f6e
Luke Mason <lukesamuelmason@gmail.com>
2022-01-16 07:49:32 -0700

    Initial Commit

---
+++ BaldwinDinners/BaldwinDinnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaldwinDinnersApp.swift
+//  BaldwinDinners
+//
+//  Created by Luke Mason on 1/16/22.
+//
+
+import SwiftUI
+
+@main
+struct BaldwinDinnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
